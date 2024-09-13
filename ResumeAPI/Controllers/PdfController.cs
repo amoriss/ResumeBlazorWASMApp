141fbf3cdb15150e74550755bb02d94c5e5077da
Amoriss <amoriss@protonmail.com>
2024-09-13 16:19:01 -0500

    added api project with resume controller

---
+++ ResumeAPI/Controllers/PdfController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResumeAPI.Controllers;
+public class PdfController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
