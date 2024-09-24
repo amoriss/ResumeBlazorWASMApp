f505d526376b326d514030e976c7c82bc189c00a
Amoriss <amoriss@protonmail.com>
2024-09-24 17:23:33 -0500

    successfully reading PDF document from local storage and display document on page in proper PDF format

--- ResumeBlazorWASMApp/obj/Debug/net8.0/ResumeBlazorWASMApp.AssemblyInfo.cs
+++ ResumeBlazorWASMApp/obj/Debug/net8.0/ResumeBlazorWASMApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResumeBlazorWASMApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3da2668604381c4f9dc96d87cb32fb980e690f52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d5379f30425f0e3555b19eb9c294cd07d986f38")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResumeBlazorWASMApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResumeBlazorWASMApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
