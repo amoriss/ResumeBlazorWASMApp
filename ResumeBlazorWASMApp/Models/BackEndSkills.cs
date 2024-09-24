7a9ecb2f5e325075f6dfa3a08e761b3f9bc95774
Amoriss <amoriss@protonmail.com>
2024-09-24 18:08:33 -0500

    added backendskills and frontendskills models and skillsfinder service class

---
+++ ResumeBlazorWASMApp/Models/BackEndSkills.cs
@@ -0,0 +1,9 @@
+﻿namespace ResumeBlazorWASMApp.Models;
+
+public class BackEndSkills
+{
+    public bool UsesCSharp { get; set; }
+    public bool UsesVisualStudio { get; set; }
+    public bool UsesAspnet { get; set; }
+    public bool UsesSql { get; set; }
+}
