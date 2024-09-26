3bbc5b90ff1ed23e457c9d02069de1d77da8d672
Amoriss <amoriss@protonmail.com>
2024-09-25 23:17:56 -0500

    fixed curly brace error and centered candidate success box

--- ResumeBlazorWASMApp/obj/Debug/net8.0/ResumeBlazorWASMApp.AssemblyInfo.cs
+++ ResumeBlazorWASMApp/obj/Debug/net8.0/ResumeBlazorWASMApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResumeBlazorWASMApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9fd72365988b648efb7d566c9135ef212103b2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+069cf21c0065b04c6ab15a398169c823a13d3e06")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResumeBlazorWASMApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResumeBlazorWASMApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
