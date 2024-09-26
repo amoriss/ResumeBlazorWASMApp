bda1af365d5e0047f91b2f4c768b3f1f5c337796
Amoriss <amoriss@protonmail.com>
2024-09-26 15:42:36 -0500

    deleting an eligible candidate from local storage now working

--- ResumeBlazorWASMApp/Models/Candidate.cs
+++ ResumeBlazorWASMApp/Models/Candidate.cs
@@ -6,4 +6,17 @@ public class Candidate
     public string LastName { get; set; }
     public string Email { get; set; }
     public string PdfResume { get; set; }
+    public override bool Equals(object obj)
+    {
+        if (obj is Candidate other)
+        {
+            return FirstName == other.FirstName && LastName == other.LastName && Email == other.Email;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(FirstName, LastName, Email);
+    }
 }
