79bdd7c7227f5e4d2dd3e2fd64369f7b25befe26
Amoriss <amoriss@protonmail.com>
2024-10-19 17:49:01 -0500

    made changes

--- ResumeAPI/obj/Debug/net8.0/ResumeAPI.AssemblyInfo.cs
+++ ResumeAPI/obj/Debug/net8.0/ResumeAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResumeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fb49c8aab3e7664c4c5cdff175a5d31e527390b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f013bd46e533e7b2062e95408c423fd0d3eeead")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResumeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResumeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
