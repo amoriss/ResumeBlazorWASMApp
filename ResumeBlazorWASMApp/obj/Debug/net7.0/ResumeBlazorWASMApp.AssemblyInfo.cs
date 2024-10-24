634d539eeef0c10c6f163ae8252cc4888de9163d
Amoriss <amoriss@protonmail.com>
2024-10-24 17:44:08 -0500

    using .net8.0

--- ResumeBlazorWASMApp/obj/Debug/net7.0/ResumeBlazorWASMApp.AssemblyInfo.cs
+++ ResumeBlazorWASMApp/obj/Debug/net7.0/ResumeBlazorWASMApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResumeBlazorWASMApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0ac72b268e4ca604d3ab1737799a0ee392a3f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0315e8f25690cc1b375d1f6a5dd78e585384825e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResumeBlazorWASMApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResumeBlazorWASMApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
