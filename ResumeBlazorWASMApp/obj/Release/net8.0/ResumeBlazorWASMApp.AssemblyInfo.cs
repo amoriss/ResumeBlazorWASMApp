c8d1be9700bfa097dfe7feb72fc9e78a696bc1ce
Amoriss <amoriss@protonmail.com>
2024-10-24 17:17:55 -0500

    deleted files

--- ResumeBlazorWASMApp/obj/Release/net8.0/ResumeBlazorWASMApp.AssemblyInfo.cs
+++ ResumeBlazorWASMApp/obj/Release/net8.0/ResumeBlazorWASMApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResumeBlazorWASMApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9389826bde93c3711900d9c184d6de252a0545c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10a05240e71058cfa2fbb06a11882138c46ed534")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResumeBlazorWASMApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResumeBlazorWASMApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
