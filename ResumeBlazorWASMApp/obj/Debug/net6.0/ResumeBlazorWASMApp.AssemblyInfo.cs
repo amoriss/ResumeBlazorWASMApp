4cab7670509480a2ad666ef674dea5e47e9f1f2c
Amoriss <amoriss@protonmail.com>
2024-10-24 19:19:34 -0500

    updated nuget packages

--- ResumeBlazorWASMApp/obj/Debug/net6.0/ResumeBlazorWASMApp.AssemblyInfo.cs
+++ ResumeBlazorWASMApp/obj/Debug/net6.0/ResumeBlazorWASMApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResumeBlazorWASMApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1680406f656a097e38292c0e9a0df7f308cdc138")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b365933f47e32ff3edbf31e265ae1e9ffa29c7dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResumeBlazorWASMApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResumeBlazorWASMApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
